2639cd43a225ea77b9245831d2e09170604f65c2
BelmansGlenn <82647919+BelmansGlenn@users.noreply.github.com>
2021-05-31 14:14:29 +0200

    Update script.js

--- Parallax1/script.js
+++ Parallax1/script.js
@@ -7,10 +7,7 @@ function parallax(e){
 
         const x = (window.innerWidth - e.pageX*speed);
         const y = (window.innerHeight - e.pageY*speed);
-        const a = (window.innerWidth + e.pageA*speed);
-        const b = (window.innerHeight + e.pageB*speed);
 
         i.style.transform = `translateX(${x/200}px) translateY(${y/200}px)`;
-        i.style.transform = `translateA(${a/200}px) translateB(${b/200}px)`;
     })
 }
